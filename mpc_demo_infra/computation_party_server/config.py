f321fe32ec3761089643b239a1a16140295c5697
mhchia <kevin.mh.chia@gmail.com>
2024-10-24 02:52:38 +0800

    try semi

--- mpc_demo_infra/computation_party_server/config.py
+++ mpc_demo_infra/computation_party_server/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     num_parties: int = 3
     party_id: int = 0
     program_bits: int = 256
-    mpspdz_protocol: str = "replicated-ring"
+    mpspdz_protocol: str = "semi"
 
     # Database settings
     database_url: str = f"sqlite:///./party_0.db"

c4ce775faf0abbbb247172310eeacf44a918f9f4
exfinen <47593166+exfinen@users.noreply.github.com>
2024-10-28 07:22:44 +0900

    fix issues

--- mpc_demo_infra/coordination_server/user_queue.py
+++ mpc_demo_infra/coordination_server/user_queue.py
@@ -1,13 +1,14 @@
 import threading
+import secrets
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple
 import time
 
 @dataclass
 class User:
     voucher_code: str
-    time_at_queue_head: Optional[int] = None
     pop_key: Optional[str] = None
+    _time_at_queue_head: Optional[int] = None
 
 class UserQueue:
     users: list[User] = []
@@ -20,37 +21,39 @@ class UserQueue:
     def _get_time() -> int:
         return int(time.time())
 
-    def _set_time_at_queue_head_if_needed(self):
-        if len(self.users) > 0 and self.users[0].time_at_queue_head is None:
-            self.users[0].time_at_queue_head = int(time.time())
+    def _set_queue_head_data_if_needed(self):
+        if len(self.users) > 0 and self.users[0]._time_at_queue_head is None:
+            user = self.users[0]
+            user._time_at_queue_head = int(time.time())
+            user.pop_key = secrets.token_urlsafe(16)
 
     def pop_user(self, pop_key: str) -> bool:
         with self.lock:
             if len(self.users) == 0 or self.users[0].pop_key != pop_key:
                 return False
             else:
-                user = users.pop(0)
-                self._set_time_at_queue_head_if_needed()
+                user = self.users.pop(0)
+                self._set_queue_head_data_if_needed()
                 return True
 
-    def get_position(self, voucher_code: str) -> int:
+    def get_position(self, voucher_code: str) -> Tuple[int, Optional[str]]:
         with self.lock:
             # if the user with voucher is not in the queue, add the user
             if not any(user.voucher_code == voucher_code for user in self.users):
                 user = User(voucher_code=voucher_code)
                 self.users.append(user)
-                self._set_time_at_queue_head_if_needed() # can be the first user
+                self._set_queue_head_data_if_needed() # can be the first user
 
             # if user[0] is staying there too long, move the user to the end
             now = UserQueue._get_time()
-            if now - self.users[0].time_at_queue_head > self.queue_head_timeout:
+            if now - self.users[0]._time_at_queue_head > self.queue_head_timeout:
                 user = self.users.pop(0)
-                user.time_at_queue_head = None
+                user._time_at_queue_head = None
                 self.users.append(user)
-                self._set_time_at_queue_head_if_needed()
+                self._set_queue_head_data_if_needed()
 
-            # return the position of the user with the voucher
+            # return the position and pop_key of the user with the voucher
             for i, user in enumerate(self.users):
                 if user.voucher_code == voucher_code:
-                    return i
+                    return i, user.pop_key
 
--- tests/test_user_queue.py
+++ tests/test_user_queue.py
@@ -1,13 +1,29 @@
 from mpc_demo_infra.coordination_server.user_queue import UserQueue
 
-def get_queue() -> UserQueue:
-    return UserQueue(max_size=10, queue_head_timeout=60)
+def get_queue(max_size: int = 10, queue_head_timeout: int=60) -> UserQueue:
+    return UserQueue(max_size=max_size, queue_head_timeout=queue_head_timeout)
 
 def test_pop_user_empty_queue():
     q = get_queue()
     assert q.pop_user('mpc') == False
 
-def test_get_position_empty_queue():
+def test_get_position_1_user():
+    q = get_queue()
+    position, _ = q.get_position('mpc')
+    assert position == 0
+
+def test_get_position_2_users():
     q = get_queue()
     position = q.get_position('mpc')
+    assert position, _ == 0
+    position = q.get_position('apple')
+    assert position, _ == 1
+
+def test_get_position_2_users_valid_pop():
+    q = get_queue()
+    position, pop_key_1 = q.get_position('mpc')
     assert position == 0
+    position, pop_key_2 = q.get_position('apple')
+    assert position == 1
+    assert q.pop_user(pop_key_1) == True
+

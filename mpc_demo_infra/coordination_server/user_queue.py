ae1763bb60a285fc38ff56239df8a1d0838a6717
exfinen <47593166+exfinen@users.noreply.github.com>
2024-11-02 17:33:30 +0900

    use user position map in user queue

--- mpc_demo_infra/coordination_server/user_queue.py
+++ mpc_demo_infra/coordination_server/user_queue.py
@@ -6,7 +6,7 @@ import time
 
 @dataclass
 class User:
-    voucher_code: str
+    access_key: str
     computation_key: Optional[str] = None
     _time_at_queue_head: Optional[int] = None
 
@@ -16,6 +16,7 @@ class UserQueue:
         self.max_size = max_size
         self.queue_head_timeout = queue_head_timeout
         self.lock = threading.Lock()
+        self.user_positions = {}
 
     def _get_time() -> int:
         return int(time.time())
@@ -26,9 +27,8 @@ class UserQueue:
             user._time_at_queue_head = int(time.time())
             user.computation_key = secrets.token_urlsafe(16)
 
-    # for debugging
-    def _get_users(self) -> list[User]:
-        return self.users
+    def build_position_map(self) -> None:
+        self.user_positions = {user.access_key: (i, user) for i, user in enumerate(self.users)}
 
     def validate_computation_key(self, computation_key: str) -> bool:
         with self.lock:
@@ -39,34 +39,37 @@ class UserQueue:
             if len(self.users) > 0 and self.users[0].computation_key == computation_key:
                 user = self.users.pop(0)
                 self._set_queue_head_data_if_needed()
+                self.build_position_map()
                 return True
             else:
                 return False
 
-    def get_position(self, voucher_code: str) -> Tuple[Optional[int], Optional[str]]:
+    # returns (postion, computations_key)
+    def get_position(self, access_key: str) -> Tuple[Optional[int], Optional[str]]:
         with self.lock:
             # reject if max_size has been reached
             if len(self.users) == self.max_size:
                 return None, None
 
-            # if the user with voucher is not in the queue, add the user
-            if not any(user.voucher_code == voucher_code for user in self.users):
-                user = User(voucher_code=voucher_code)
-                self.users.append(user)
-
-            # if user at queue head times out, move the user to the end
-            if self.users[0]._time_at_queue_head is not None:
+            # if user at queue head times out, remove the user
+            if len(self.users) > 0 and self.users[0]._time_at_queue_head is not None:
                 queue_head_time = UserQueue._get_time() - self.users[0]._time_at_queue_head
                 if queue_head_time > self.queue_head_timeout:
                     user = self.users.pop(0)
-                    user._time_at_queue_head = None
-                    user.computation_key = None
-                    self.users.append(user)
+                    self.build_position_map()
+
+            position, user = self.user_positions.get(access_key, (None, None))
+
+            # if the user is not in the queue, add the user
+            if position is None:
+                user = User(access_key=access_key)
+                self.users.append(user)
+                rebuild_position_map = True
+                position = len(self.users) - 1
+                self.build_position_map()
 
             self._set_queue_head_data_if_needed()
+            position, user = self.user_positions[access_key]
 
-            # return the position and computation_key of the user with the voucher
-            for i, user in enumerate(self.users):
-                if user.voucher_code == voucher_code:
-                    return i, user.computation_key
+            return (position, None if position != 0 else user.computation_key)
 

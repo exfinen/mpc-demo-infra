4d8fcc108ead2d1a103fba24cc091caf091b1ad2
exfinen <47593166+exfinen@users.noreply.github.com>
2024-11-03 14:17:05 +0900

    wip

--- mpc_demo_infra/coordination_server/user_queue.py
+++ mpc_demo_infra/coordination_server/user_queue.py
@@ -28,12 +28,13 @@ class UserQueue:
         self.locker = rwlock.RWLockWrite()
 
     def _queue_to_str(self) -> str:
-        users = []
-        user = self.users_head
-        while user is not None:
-            users.append(user.access_key)
-            user = user.next
-        return ','.join(users)
+        with self.locker.gen_rlock():
+            users = []
+            user = self.users_head
+            while user is not None:
+                users.append(user.access_key)
+                user = user.next
+            return ','.join(users)
     
     def _add_user(self, user: User) -> None:
         if self.users_head == None:

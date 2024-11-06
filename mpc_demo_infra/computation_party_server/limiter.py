cfa2f982b9787745764233df806a41769d6869df
exfinen <47593166+exfinen@users.noreply.github.com>
2024-11-06 11:02:12 +0700

    wip

--- mpc_demo_infra/computation_party_server/limiter.py
+++ mpc_demo_infra/computation_party_server/limiter.py
@@ -1,4 +1,4 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["2000/minute"])
+limiter = Limiter(key_func=get_remote_address, default_limits=["30/minute"])

ba15475097a15277c70718265ecb3e7967b0b734
exfinen <47593166+exfinen@users.noreply.github.com>
2024-11-11 14:45:19 +0700

    refined docs

--- mpc_demo_infra/client_cli/docker/share-data.sh
+++ mpc_demo_infra/client_cli/docker/share-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <voucher> <api-key> <api-secret>"
+    echo "Usage: $0 <voucher> <binance-api-key> <binance-api-secret>"
     exit 1
 fi
 docker run -it client_cli client-share-data $1 $2 $3

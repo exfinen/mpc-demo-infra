db45fd0865f71486e3d2c4c51ccb3a0ce1b4892b
exfinen <47593166+exfinen@users.noreply.github.com>
2024-11-12 08:17:48 +0700

    use images as much as possible

--- mpc_demo_infra/client_cli/docker/share-data.sh
+++ mpc_demo_infra/client_cli/docker/share-data.sh
@@ -4,5 +4,15 @@ if [ "$#" -lt 3 ]; then
     echo "Usage: $0 <voucher> <binance-api-key> <binance-api-secret>"
     exit 1
 fi
-docker run -it client_cli client-share-data $1 $2 $3
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    TAG="latest-arm64"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    TAG="latest-amd64"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
+
+docker run -it mpcstats/client_cli:$TAG client-share-data $1 $2 $3
 

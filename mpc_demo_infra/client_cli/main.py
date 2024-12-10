1cf29015df3c8e95e2d83114b6470eb503b678c0
exfinen <47593166+exfinen@users.noreply.github.com>
2024-12-10 18:16:24 +0900

    use timestamp as a client_id in client_cli

--- mpc_demo_infra/client_cli/main.py
+++ mpc_demo_infra/client_cli/main.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import json
 import secrets
 import logging
+from datetime import datetime
 
 from ..client_lib.lib import fetch_parties_certs, share_data, query_computation, add_user_to_queue, poll_queue_until_ready
 from .config import settings
 from ..logger_config import configure_console_logger
+from ..constants import MAX_CLIENT_ID
 
 configure_console_logger()
 logger = logging.getLogger(__name__)
@@ -39,9 +41,10 @@ async def notarize_and_share_data(eth_address: str, api_key: str, api_secret: st
         logger.info(f"Found binance_prover in {binance_prover_dir}")
 
     # Gen tlsn proofs
-    logger.info(f"Generating Binance ETH balance TLSN proof...")
-    proof_file = PROJECT_ROOT / f"proof.json"
-    secret_file = PROJECT_ROOT/ f"secret.json"
+    logger.info(f"Generating Binance ETH balance TLSN proof with notary server {settings.notary_server_host}:{settings.notary_server_port}...")
+    timestamp = int(datetime.now().timestamp() * 10) % MAX_CLIENT_ID
+    proof_file = PROJECT_ROOT / f"proof{timestamp}.json"
+    secret_file = PROJECT_ROOT/ f"secret{timestamp}.json"
 
     process = await asyncio.create_subprocess_shell(
         f"cd {binance_prover_dir} && {binance_prover_exec_cmd} {settings.notary_server_host} {settings.notary_server_port} {api_key} {api_secret} {str(proof_file.resolve())} {str(secret_file.resolve())}",
@@ -51,6 +54,8 @@ async def notarize_and_share_data(eth_address: str, api_key: str, api_secret: st
     stdout, stderr = await process.communicate()
     if process.returncode != 0:
         raise Exception(f"TLSN proof generation failed with return code {process.returncode}, {stdout=}, {stderr=}")
+    else:
+        logger.info(f"Generated TLSN proof")
     with open(proof_file, "r") as f:
         tlsn_proof = f.read()
     with open(secret_file, "r") as f_secret:
@@ -81,6 +86,7 @@ async def notarize_and_share_data(eth_address: str, api_key: str, api_secret: st
         secret_input,
         nonce,
         computation_key,
+        timestamp,
     )
     logger.info(f"Binance ETH balance data has been shared secretly to MPC parties.")
 

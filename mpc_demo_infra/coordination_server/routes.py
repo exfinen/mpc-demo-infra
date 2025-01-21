d761fa6feaa12a11be94edbb3e6e73bc7f7124ca
exfinen <47593166+exfinen@users.noreply.github.com>
2025-01-21 13:12:26 +0900

    wip

--- mpc_demo_infra/coordination_server/routes.py
+++ mpc_demo_infra/coordination_server/routes.py
@@ -115,11 +115,10 @@ async def share_data(request: RequestSharingDataRequest, x: Request, db: Session
 
     # Verify TLSN proof.
     with tempfile.NamedTemporaryFile(delete=False) as temp_tlsn_proof_file:
-        tlsn_proof = request.tlsn_proof.encode('utf-8')
-        logger.info(f"TLSN proof: {tlsn_proof}")
+        logger.info(f"TLSN proof: {request.tlsn_proof}")
         logger.info(f"Writing TLSN proof to temporary file: {temp_tlsn_proof_file.name}")
         # Store TLSN proof in temporary file.
-        temp_tlsn_proof_file.write(tlsn_proof)
+        temp_tlsn_proof_file.write(request.tlsn_proof.encode('utf-8'))
 
         logger.info(f"Executing TLSN proof verifier with: {CMD_VERIFY_TLSN_PROOF} {temp_tlsn_proof_file.name}")
         # Run TLSN proof verifier
@@ -137,22 +136,22 @@ async def share_data(request: RequestSharingDataRequest, x: Request, db: Session
         )
         logger.info(f"Getting TLSN proof verification result...")
         stdout, stderr = await process.communicate()
-        try:
-            uid = get_uid_from_tlsn_proof_verifier(stdout.decode('utf-8'))
-            logger.info(f"Got UID from TLSN proof verifier: {uid}")
-        except ValueError as e:
-            logger.error(f"Failed to get UID from TLSN proof verifier: {e}, {stdout.decode('utf-8')=}, {stderr.decode('utf-8')=}")
-            raise HTTPException(status_code=400, detail="Failed to get UID from TLSN proof verifier")
-        if process.returncode != 0:
-            logger.error(f"TLSN proof verification failed with return code {process.returncode}, {stdout=}, {stderr=}")
-            raise HTTPException(status_code=400, detail=f"TLSN proof verification failed with return code {process.returncode}, {stdout=}, {stderr=}")
-        logger.info(f"TLSN proof verification passed")
+        if settings.prohibit_multiple_contributions:
+            try:
+                uid = get_uid_from_tlsn_proof_verifier(stdout.decode('utf-8'))
+                logger.info(f"Got UID from TLSN proof verifier: {uid}")
+            except ValueError as e:
+                logger.error(f"Failed to get UID from TLSN proof verifier: {e}, {stdout.decode('utf-8')=}, {stderr.decode('utf-8')=}")
+                raise HTTPException(status_code=400, detail="Failed to get UID from TLSN proof verifier")
+            if process.returncode != 0:
+                logger.error(f"TLSN proof verification failed with return code {process.returncode}, {stdout=}, {stderr=}")
+                raise HTTPException(status_code=400, detail=f"TLSN proof verification failed with return code {process.returncode}, {stdout=}, {stderr=}")
+            logger.info(f"TLSN proof verification passed")
 
-    # Check if uid already in db. If so, raise an error.
-    if settings.prohibit_multiple_contributions:
-        if db.query(MPCSession).filter(MPCSession.uid == uid).first():
-            logger.error(f"UID {uid} already in database")
-            raise HTTPException(status_code=400, detail=f"UID {uid} already shared data")
+            # Check if uid already in db. If so, raise an error.
+            if db.query(MPCSession).filter(MPCSession.uid == uid).first():
+                logger.error(f"UID {uid} already in database")
+                raise HTTPException(status_code=400, detail=f"UID {uid} already shared data")
 
     # Acquire lock to prevent concurrent sharing data requests
     logger.info(f"Acquiring lock for sharing data for {eth_address=}")
